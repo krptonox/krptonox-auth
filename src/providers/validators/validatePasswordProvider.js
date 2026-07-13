dd96a1456aeb1e6e6aea82bfc79f73f154a059aa
krptonox <aryanyadav182007@gmail.com>
2026-07-13 11:23:44 +0530

    refactor: rename variable to uppercase for constant in validatePasswordProvider function

--- src/providers/validators/validatePasswordProvider.js
+++ src/providers/validators/validatePasswordProvider.js
@@ -2,16 +2,16 @@ import ValidationError from "../errors/ValidationError.js";
 
 export function validatePasswordProvider(passwordProvider) {
     
-    const requiredMethods = {
-        hash: "hash(password)",
-        verify: "verify(password, hash)"
-    };
+    const REQUIRED_PASSWORD_METHODS = {
+         hash: "hash(password)",
+         verify: "verify(password, hash)"
+   };
 
     if(passwordProvider === null || passwordProvider === undefined){
         throw new ValidationError("Password provider is required");
     }
     
-    for (const [method, signature] of Object.entries(requiredMethods)) {
+    for(const [method, signature] of Object.entries(REQUIRED_PASSWORD_METHODS)){
 
     if(typeof passwordProvider[method] !== "function"){
 
